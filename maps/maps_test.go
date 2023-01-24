d07759eb7b69910ca97920f785d2bbcfbfe2076e
Elephant OSS <spirit@elephantoss.io>
2023-01-24 15:03:50 -0800

    test (maps): implement test table with specific tests

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -24,3 +24,39 @@ func TestFind(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestFind_Table(t *testing.T) {
+	cases := []struct {
+		title         string
+		input_map     map[string]int
+		key           string
+		default_value int
+		expectation   int
+	}{
+		{
+			title:         "return default if key is not in the map",
+			input_map:     map[string]int{"a": 10},
+			key:           "b",
+			default_value: 50,
+			expectation:   50,
+		},
+		{
+			title:         "return value from map if key is in the map",
+			input_map:     map[string]int{"a": 10},
+			key:           "a",
+			default_value: 50,
+			expectation:   10,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.title, func(t *testing.T) {
+			actual := maps.Find(tc.input_map, tc.key, tc.default_value)
+			if actual != tc.expectation {
+				t.Fatalf(
+					"%s: expected: %d got: %d",
+					tc.title, tc.expectation, actual)
+			}
+		})
+	}
+}
